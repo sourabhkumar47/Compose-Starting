06cb78514512d1c21e1cc7dd677d9f2f5ac82c61
Sourabh Kumar <20010001030@gateway.edu.in>
2022-09-18 00:16:05 +0530

    Created column

--- app/src/main/java/com/example/composeintro/MainActivity.kt
+++ app/src/main/java/com/example/composeintro/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.composeintro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.magnifier
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -28,20 +29,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!",Modifier.padding(24.dp))
+    Surface(color = MaterialTheme.colors.primary,
+    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
+            Text(text = "Hello,")
+            Text(text = name)
+        }
+
     }
 }
 
 @Composable
-private fun MyApp(){
-    Surface(color = MaterialTheme.colors.background){
-        Greeting("Compose")
+private fun MyApp(names: List<String> = listOf("World", "Sourabh", "Compose", "Kotlin")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+
     }
 
 }
 
-@Preview(showBackground = true, name = "Text Preview")
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     ComposeIntroTheme {
