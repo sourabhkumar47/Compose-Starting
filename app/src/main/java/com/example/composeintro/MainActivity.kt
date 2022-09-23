55288086ca137d212ed7d862c57fe67ff7495783
Sourabh Kumar <20010001030@gateway.edu.in>
2022-09-23 20:48:24 +0530

    Added dark Mode color

--- app/src/main/java/com/example/composeintro/MainActivity.kt
+++ app/src/main/java/com/example/composeintro/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composeintro
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -124,6 +125,12 @@ private fun Greeting(name: String) {
         }
     }
 }
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
